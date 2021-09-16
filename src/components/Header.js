1d976c51e5d7cd729a974b897ed263fd2eff6f37
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-09-16 21:40:52 +0900

    simply Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Box from "@mui/material/Box";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { deepPurple } from "@mui/material/colors";
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#6200EE",
-        },
-    },
-});
 
 const Header = () => {
-    // const activeStyle = { color: "#F15B2A" };
+    const headerStyle = {
+        backgroundColor: "#6200EE",
+        top: "0px",
+        left: "0px",
+        width: "1920px",
+        height: "93px",
+    };
+
+    const headerStyle2 = {
+        color: "#FFFFFF",
+        top: "10px",
+        left: "140px",
+        width: "398px",
+        height: "67px",
+        textAlign: "center",
+        fontSize: "53px",
+        position: "relative",
+        fontFamily: "Arial",
+        fontWeight: "bold 0.8",
+    };
     return (
-        <>
-            <ThemeProvider theme={theme}>
-                <AppBar position="static">
-                    <Toolbar
-                        variant="dense"
-                        sx={{
-                            height: "4rem",
-                        }}
-                    >
-                        <Typography
-                            component="div"
-                            sx={{
-                                flexGrow: 1,
-                                paddingX: "8rem",
-                                paddingY: "19px",
-                                fontSize: "2rem",
-                                textAlign: "center",
-                                position: "absolute",
-                            }}
-                        >
-                            Redesigned-Tabi
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </ThemeProvider>
-        </>
+        <div style={headerStyle}>
+            <div style={headerStyle2}>Redesigned-Tabi</div>
+        </div>
     );
 };
 
