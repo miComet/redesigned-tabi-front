4360c6a2c20ca5402e33f83a7f8d9a66a8584e12
Randy, Huang <gg875437@gmail.com>
2022-02-02 23:29:08 +0900

    create info page

---
+++ pages/info.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Info = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Info;
