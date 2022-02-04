3da22e3ac80715ebf113357d8ccf91815ca429ff
Randy, Huang <gg875437@gmail.com>
2022-02-04 23:46:36 +0900

    create PictureSelect components

---
+++ components/PictureSelect.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+
+const PictureSelect = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mx-auto -ml-24">
+      <div className="flex flex-col m-4 ml-20">
+        <h1 className="text-xl font-bold -ml-16 p-8">
+          Choose Your Favorite One
+        </h1>
+        <div className="grid grid-flow-row grid-cols-2 grid-rows-2">
+          <div className="relative h-48 w-48 mx-3 my-2">
+            <Image
+              className="rounded-lg"
+              src="/banner.jpg"
+              layout="fill"
+              objectFit="fill"
+            />
+            <div
+              className="absolute bottom-0 w-full h-1/5 bg-gray-400
+              rounded-b-lg opacity-90"
+            ></div>
+          </div>
+          <div className="relative h-48 w-48 mx-3 my-2">
+            <Image
+              className="rounded-lg"
+              src="/banner.jpg"
+              layout="fill"
+              objectFit="fill"
+            />
+            <div
+              className="absolute bottom-0 w-full h-1/5 bg-gray-400
+              rounded-b-lg opacity-90"
+            ></div>
+          </div>
+          <div className="relative h-48 w-48 mx-3 my-2">
+            <Image
+              className="rounded-lg"
+              src="/banner.jpg"
+              layout="fill"
+              objectFit="fill"
+            />
+            <div
+              className="absolute bottom-0 w-full h-1/5 bg-gray-400
+              rounded-b-lg opacity-90"
+            ></div>
+          </div>
+          <div className="relative h-48 w-48 mx-3 my-2">
+            <Image
+              className="rounded-lg"
+              src="/banner.jpg"
+              layout="fill"
+              objectFit="fill"
+            />
+            <div
+              className="absolute bottom-0 w-full h-1/5 bg-gray-400
+              rounded-b-lg opacity-90"
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      <h3 className="border rounded-lg px-8 ml-16 py-1.5 text-sky-500 border-sky-500 cursor-pointer">
+        Shuffle
+      </h3>
+    </div>
+  );
+};
+
+export default PictureSelect;
--- pages/info.tsx
+++ pages/info.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import PictureSelect from "../components/PictureSelect";
 import Userinfo from "../components/Userinfo";
 
 const Info = () => {
   return (
     <div>
       <Header />
-      <Userinfo />
+      {/* <Userinfo /> */}
+      <PictureSelect />
       <Footer />
     </div>
   );
