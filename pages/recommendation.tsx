d8e71da664a3bb8f938bd1376f0b69b785a34c8d
Randy, Huang <gg875437@gmail.com>
2022-02-06 23:34:28 +0900

    ssr fetch data and parse recommendation

--- pages/recommendation.tsx
+++ pages/recommendation.tsx
@@ -1,8 +1,15 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Image from "next/image";
+import { picRecommendation } from "../typings";
+import { useRouter } from "next/router";
 
-const Recommendation = () => {
+interface Props {
+  spotRecommendations: picRecommendation[];
+}
+
+const Recommendation = ({ spotRecommendations }: Props) => {
+  const router = useRouter();
   return (
     <div>
       <Header />
@@ -13,23 +20,32 @@ const Recommendation = () => {
       >
         <div className="flex flex-col m-4 ml-20">
           <h1 className="text-2xl font-extrabold -ml-16 p-8">Here You Go</h1>
-          <div className="flex items-center space-x-2">
-            <div className="relative h-24 w-40 mx-3 my-2">
-              <Image
-                className="rounded-lg"
-                src="/banner.jpg"
-                layout="fill"
-                objectFit="fill"
+          {spotRecommendations.map((spot) => (
+            <div
+              key={spot.image_url}
+              className="flex items-center space-x-2 justify-between"
+            >
+              <div className="relative h-24 w-40 mx-3 my-2">
+                <Image
+                  className="rounded-lg"
+                  src={spot.image_url}
+                  layout="fill"
+                  objectFit="fill"
+                />
+              </div>
+              <label className="pr-20 cursor-pointer" htmlFor={spot.image_url}>
+                {spot.title}
+              </label>
+              <label className="pr-10 cursor-pointer" htmlFor={spot.image_url}>
+                {spot.location}
+              </label>
+              <input
+                className="accent-sky-600"
+                id={spot.image_url}
+                type="checkbox"
               />
             </div>
-            <label className="pr-20 cursor-pointer" htmlFor="checkbox1">
-              Destination
-            </label>
-            <label className="pr-10 cursor-pointer" htmlFor="checkbox1">
-              Prefecture 1
-            </label>
-            <input className="accent-sky-600" id="checkbox1" type="checkbox" />
-          </div>
+          ))}
         </div>
         <div className="flex items-center space-x-2 ml-24">
           <h3
@@ -39,6 +55,7 @@ const Recommendation = () => {
             Save
           </h3>
           <h3
+            onClick={() => router.push("/info")}
             className="border rounded-lg px-12 py-1.5 text-sky-500 border-sky-500 cursor-pointer
           hover:scale-105 transform transition duration-100 ease-out"
           >
@@ -52,3 +69,14 @@ const Recommendation = () => {
 };
 
 export default Recommendation;
+
+export const getServerSideProps = async () => {
+  const spotRecommendations = await fetch(
+    "http://localhost:8000/spot_recommendation?spot_num=5"
+  ).then((res) => res.json());
+  return {
+    props: {
+      spotRecommendations,
+    },
+  };
+};
--- typings.d.ts
+++ typings.d.ts
@@ -5,3 +5,11 @@ export interface Spot {
     location: string,
     season: string
 }
+
+export interface picRecommendation {
+    id: number,
+    image_url: string,
+    title: string,
+    location: string,
+    destination_url: string
+}
