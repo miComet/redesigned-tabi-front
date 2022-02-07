c168be7456a56da52f68348250810f2201928e66
Randy, Huang <gg875437@gmail.com>
2022-02-07 23:17:45 +0900

    customize signin page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -18,22 +18,29 @@ const signIn = ({
         className="flex flex-col items-center justify-center min-h-screen
       py-2 -mt-20 px-14 text-center"
       >
-        <img
-          className="w-80"
-          src="https://pbs.twimg.com/media/FFmSlJXUYAcdG_k?format=jpg"
-          alt="logo"
-        />
-        <p className="font-xs italic">Sign in with your social account</p>
-        <div className="mt-40">
+        <p className="font-bold text-xl -ml-52">Sign up for a free account</p>
+        <div className="items-center space-x-6 mt-10">
+          <input
+            className="border-2 border-gray-400 px-4 py-2 outline-none w-80"
+            type="text"
+            placeholder="Email address"
+          />
+          <button className=" whitespace-nowrap py-2 px-8 bg-sky-500 rounded-lg text-white">
+            Sign up
+          </button>
+        </div>
+        <p className="font-xs text-sm mt-8">or</p>
+
+        <div className="mt-8">
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button
-                className="p-3 bg-blue-500 rounded-lg text-white"
+                className="py-2 px-8 text-sky-500 rounded-lg border-sky-500 border"
                 onClick={() =>
                   SighIntoProvider(provider.id, { callbackUrl: "/" })
                 }
               >
-                Sign in with {provider.name}
+                Continue with {provider.name}
               </button>
             </div>
           ))}
